e6a90e5e83cb401f374009fd4b1e39286c68ed76
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-05-13 14:36:07 +0700

    Filtering, State and QueryStrings

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,19 +1,36 @@
 <script setup>
 
-import {Link} from "@inertiajs/vue3";
+import {Link, router} from "@inertiajs/vue3";
+import { ref, watch } from "vue";
 import Layout from "../Shared/Layout.vue";
-import Pagination from "@/Shared/Pagination.vue";
+import Pagination from "../Shared/Pagination.vue";
 
-defineProps({
+const props = defineProps({
     users: Object,
+    filters: Object,
+});
+
+let search = ref(props.filters.search);
+
+watch(search, value => {
+    router.get('/users', { search: value }, { preserveState: true, replace: true });
 });
 </script>
 
 <template>
     <Layout>
-        <h1 class="text-4xl font-bold text-red-400">
-            Users page
-        </h1>
+        <div class="flex w-full justify-between items-center mb-4">
+            <h1 class="text-4xl font-bold text-red-400">
+                Users page
+            </h1>
+
+            <input
+                type="text"
+                placeholder="Search...."
+                class="px-6 py-2 text-lg bg-gray-200 rounded-full focus:outline-none"
+                v-model="search"
+            />
+        </div>
 
         <div class="overflow-x-auto mt-4">
             <table class="min-w-full bg-white border border-gray-200 w-full">
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,17 @@ Route::get('/users', function () {
 
 //     sleep(2);
     return inertia('Users', [
-        'users' => User::paginate(10)->through(fn($user) => [
-            'id' => $user->id,
-            'name' => $user->name,
-        ])
+        'users' => User::query()
+            ->when(Request::input('search'), function ($query, $search) {
+                $query->where('name', 'like', "%{$search}%");
+            })
+            ->paginate(10)
+            ->withQueryString()
+            ->through(fn($user) => [
+                'id' => $user->id,
+                'name' => $user->name,
+            ]),
+        'filters' => Request::only(['search'])
     ]);
 });
 
